ca979f06c8da591e6158768957888f476bdc1ac6
James Greenhill <fuziontech@gmail.com>
2020-09-29 17:02:39 +0100

    type annotations

--- posthog/__init__.py
+++ posthog/__init__.py
@@ -104,7 +104,7 @@ def alias(
 def feature_enabled(
     key, # type: str,
     distinct_id, # type: str,
-    default=False, # type: Optional[Any]
+    default=False, # type: bool 
     ):
     # type: (...) -> bool
     """
