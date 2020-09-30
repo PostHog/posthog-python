b05ee3884a5b4ec8010953afffca74f2229ea826
James Greenhill <fuziontech@gmail.com>
2020-09-30 13:53:47 +0100

    Send distinctID to the decide endpoint to determin if user should have features enabled

--- posthog/request.py
+++ posthog/request.py
@@ -15,12 +15,13 @@ _session = requests.sessions.Session()
 DEFAULT_HOST = 'https://app.posthog.com'
 USER_AGENT = 'posthog-python/' + VERSION
 
-def post(api_key, host=None, gzip=False, timeout=15, **kwargs):
+
+def post(api_key, host=None, path=None, gzip=False, timeout=15, **kwargs):
     """Post the `kwargs` to the API"""
     log = logging.getLogger('posthog')
     body = kwargs
     body["sentAt"] = datetime.utcnow().replace(tzinfo=tzutc()).isoformat()
-    url = remove_trailing_slash(host or DEFAULT_HOST) + '/batch/'
+    url = remove_trailing_slash(host or DEFAULT_HOST) + path
     body['api_key'] = api_key
     data = json.dumps(body, cls=DatetimeSerializer)
     log.debug('making request: %s', data)
@@ -44,12 +45,37 @@ def post(api_key, host=None, gzip=False, timeout=15, **kwargs):
         log.debug('data uploaded successfully')
         return res
 
+
+def decide(api_key, host=None, gzip=False, timeout=15, **kwargs):
+    """Post the `kwargs to the decide API endpoint"""
+    log = logging.getLogger('posthog')
+    res = post(api_key, host, '/decide/', gzip, timeout, **kwargs)
+    if res.status_code == 200:
+        log.debug('Feature flags decided successfully')
+        return res
     try:
         payload = res.json()
         log.debug('received response: %s', payload)
         raise APIError(res.status_code, payload['detail'])
     except ValueError:
         raise APIError(res.status_code, res.text)
+
+
+def batch_post(api_key, host=None, gzip=False, timeout=15, **kwargs):
+    """Post the `kwargs` to the batch API endpoint for events"""
+    log = logging.getLogger('posthog')
+    res = post(api_key, host, '/batch/', gzip, timeout, **kwargs)
+
+    if res.status_code == 200:
+        log.debug('data uploaded successfully')
+        return res
+    try:
+        payload = res.json()
+        log.debug('received response: %s', payload)
+        raise APIError(res.status_code, payload['detail'])
+    except ValueError:
+        raise APIError(res.status_code, res.text)
+
 
 def get(api_key, url, host=None, timeout=None):
     log = logging.getLogger('posthog')
