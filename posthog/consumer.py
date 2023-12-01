6eb37a48486a0bb523193b06b044e606d428c7e8
Xavier Vello <xavier@posthog.com>
2023-12-01 17:24:46 +0100

    fmt

--- posthog/consumer.py
+++ posthog/consumer.py
@@ -17,7 +17,7 @@ MAX_MSG_SIZE = 900 * 1024  # 900KiB per event
 
 # The maximum request body size is currently 20MiB, let's be conservative
 # in case we want to lower it in the future.
-BATCH_SIZE_LIMIT = 5*1024*1024
+BATCH_SIZE_LIMIT = 5 * 1024 * 1024
 
 
 class Consumer(Thread):
