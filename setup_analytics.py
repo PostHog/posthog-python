c0e2951f9ffb779f82b13c90c004c2a85cf4bbaf
Georgiy Tarasov <gtarasov.work@gmail.com>
2025-01-14 10:45:27 +0100

    feat: add new packages for posthoganalytics

--- setup_analytics.py
+++ setup_analytics.py
@@ -30,6 +30,8 @@ setup(
     packages=[
         "posthoganalytics",
         "posthoganalytics.ai",
+        "posthoganalytics.ai.langchain",
+        "posthoganalytics.ai.openai",
         "posthoganalytics.test",
         "posthoganalytics.sentry",
         "posthoganalytics.exception_integrations",
@@ -59,5 +61,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
