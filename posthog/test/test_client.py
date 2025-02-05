5d73f272a458828638c4e34e75520c5249174400
Phil Haack <haacked@gmail.com>
2025-02-05 19:22:13 +0900

    Fix unit test

--- posthog/test/test_client.py
+++ posthog/test/test_client.py
@@ -743,14 +743,12 @@ class TestClient(unittest.TestCase):
             },
         )
         self.assertEqual(msg["timestamp"], "2014-09-03T00:00:00+00:00")
-        self.assertEqual(msg["context"]["ip"], "192.168.0.1")
 
     def test_advanced_group_identify_with_distinct_id(self):
         success, msg = self.client.group_identify(
             "organization",
             "id:5",
             {"trait": "value"},
-            {"ip": "192.168.0.1"},
             datetime(2014, 9, 3),
             "new-uuid",
             distinct_id="distinct_id",
