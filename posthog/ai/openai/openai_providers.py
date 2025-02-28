79547f773e5555cd98ab03aeedbefa5c7e57b0bb
Peter Kirkham <peter@posthog.com>
2025-02-28 12:20:54 -0800

    fix: isort

--- posthog/ai/openai/openai_providers.py
+++ posthog/ai/openai/openai_providers.py
@@ -5,11 +5,9 @@ except ImportError:
     raise ModuleNotFoundError("Please install the Open AI SDK to use this feature: 'pip install openai'")
 
 from posthog.ai.openai.openai import WrappedBeta, WrappedChat, WrappedEmbeddings
-from posthog.ai.openai.openai_async import (
-    WrappedBeta as AsyncWrappedBeta,
-    WrappedChat as AsyncWrappedChat,
-    WrappedEmbeddings as AsyncWrappedEmbeddings,
-)
+from posthog.ai.openai.openai_async import WrappedBeta as AsyncWrappedBeta
+from posthog.ai.openai.openai_async import WrappedChat as AsyncWrappedChat
+from posthog.ai.openai.openai_async import WrappedEmbeddings as AsyncWrappedEmbeddings
 from posthog.client import Client as PostHogClient
 
 
