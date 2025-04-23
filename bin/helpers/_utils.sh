789160f03ae2443e9fbf7829a82c9350ff407a70
Phil Haack <haacked@gmail.com>
2025-04-23 12:54:43 -0700

    Add parameters to `bin/test`
    And make sure we activate properly.

--- bin/helpers/_utils.sh
+++ bin/helpers/_utils.sh
@@ -13,3 +13,14 @@ set_source_and_root_dir() {
     root_dir=$(cd "$source_dir" && cd ../ && pwd)
     cd "$root_dir"
 }
+
+ensure_virtual_env() {
+    if [ -z "$VIRTUAL_ENV" ]; then
+        echo "Virtual environment not activated. Activating now..."
+        if [ ! -f env/bin/activate ]; then
+            echo "Virtual environment not found. Please run 'python -m venv env' first."
+            exit 1
+        fi
+        source env/bin/activate
+    fi
+}
