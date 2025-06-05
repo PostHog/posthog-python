d149e8041a044d5f351f98d03cff367f836f0dee
Oliver Browne <oliver@posthog.com>
2025-06-05 14:19:34 +0300

    formatting

--- posthog/scopes.py
+++ posthog/scopes.py
@@ -6,6 +6,7 @@ from typing import Any, Callable, Dict, TypeVar, cast
 # Use contextvars if available (Python 3.7+), otherwise fall back to threading.local
 if sys.version_info >= (3, 7):
     import contextvars
+
     _context_stack: contextvars.ContextVar[list] = contextvars.ContextVar("posthog_context_stack", default=[{}])
     _use_contextvars = True
 else:
