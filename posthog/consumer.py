4326ab2b28a7d1ddd29f19f531c3bbc2009fb5f6
Masen Furer <m_github@0x26.net>
2025-08-18 12:33:19 -0700

    consumer.py: mark task_done for discarded items
    Fixes #308

--- posthog/consumer.py
+++ posthog/consumer.py
@@ -110,6 +110,7 @@ class Consumer(Thread):
                     self.log.error(
                         "Item exceeds 900kib limit, dropping. (%s)", str(item)
                     )
+                    queue.task_done()
                     continue
                 items.append(item)
                 total_size += item_size
