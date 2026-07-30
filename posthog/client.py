59b3ca5064a78e052e83367eaccaf64610aed641
Manoel Aranda Neto <marandaneto@gmail.com>
2026-07-30 16:45:23 +0200

    fix(flags): use device ID in local evaluation

--- posthog/client.py
+++ posthog/client.py
@@ -3373,6 +3373,7 @@ class Client(object):
             person_properties=local_person_properties,
             group_properties=group_properties,
             flag_keys_to_evaluate=flag_keys,
+            device_id=device_id,
         )
 
         feature_flags_by_key: Dict[str, Any] = self.feature_flags_by_key or {}
